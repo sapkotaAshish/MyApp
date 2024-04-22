c26d183158c935fdbe2c7edcd5d4a0db076a41f6
Ashish Sapkota <sapkotaashish1923@gmail.com>
2024-04-22 00:22:35 -0400

    added contact page (test)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
+//motion
+import { motion } from "framer-motion";
+//variants
+import { fadeIn } from "../variants";
 
 const Contact = () => {
-  return <div id='contact'>Contact</div>;
+  return (
+    <section className="section" id="contact">
+      <div className="container mx-auto">
+        <div>1</div>
+        <div>2</div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
